81a79342e3228a88b1f0bb1a9976d4bcf651528b
niels <niels@vrsafe.de>
2019-03-22 21:21:52 +0100

    Beispielszene mit ersten Buttons

---
+++ Assets/Scripts/ChangeColour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeColourButton()
+    {
+        this.gameObject.SetActive(false);
+        //this.GetComponent<Button>().colors.normalColor = Color.red;
+    }
+}
