f7870ebfa9c7d8ad22d06b706bfcbf06d38f1f76
Siedlerchr <siedlerkiller@gmail.com>
2019-03-23 18:40:57 +0100

    add right led view for FW control panel

--- Assets/Scripts/FWControlPanel/rightLEDView.cs
+++ Assets/Scripts/FWControlPanel/rightLEDView.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class rightLEDView : MonoBehaviour
 {
+    public GameObject panelUEexecuted;
+    public GameObject panelBrandfallControlAb;
+    public GameObject panelLEDAcoustigSignal;
+    public GameObject panelBMZReset;
+
+    private Image imageUEExecuted;
+    private Image imageBrandFallControlAb;
+    private Image imageLEDAcousticSignal;
+    private Image imageBMZReset;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        imageUEExecuted = panelUEexecuted.GetComponent<Image>();
+        imageBrandFallControlAb = panelBrandfallControlAb.GetComponent<Image>();
+        imageLEDAcousticSignal = panelLEDAcoustigSignal.GetComponent<Image>();
+        imageBMZReset = panelBMZReset.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -15,4 +30,32 @@ public class rightLEDView : MonoBehaviour
     {
         
     }
+
+    public void switchLEDUEExecutedOn()
+    {
+        imageUEExecuted.color = Color.red;
+
+    }
+    public void switchLEDUEExecutedOff()
+    {
+        imageUEExecuted.color = Color.grey;
+    }
+    public void switchLDBrandFallControlAbOn()
+    {
+        imageBrandFallControlAb.color = Color.yellow;
+    }
+    public void switchLEDBrandFallControlAbOff()
+    {
+        imageBrandFallControlAb.color = Color.grey;
+    }
+
+    public void switchImageMZResetOn()
+    {
+        imageBMZReset.color = Color.red;
+    }
+    public void switchBMZResetOff()
+    {
+        imageBMZReset.color = Color.grey;
+    }
+
 }
