0a3447d1ef7431548b9d8e46a34717c08f2123d9
Siedlerchr <siedlerkiller@gmail.com>
2019-03-23 15:16:26 +0100

    add possibility to play two sounds

---
+++ Assets/Scripts/ButtonMultipleSoundsView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonMultipleSoundsView : MonoBehaviour, IPointerClickHandler
+{
+    public AudioSource AudioSource;  
+    public AudioClip ClickSound;
+    public AudioClip secondClip;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PlayClickSound();
+    }
+
+    private void PlayClickSound()
+    {
+        AudioSource.PlayOneShot(ClickSound);
+        AudioSource.PlayOneShot(secondClip);
+    }
+}
