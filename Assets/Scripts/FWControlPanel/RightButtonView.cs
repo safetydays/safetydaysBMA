38c3f9f6cf02363a7ded1aa02e2e97dcf0457dd9
Siedlerchr <siedlerkiller@gmail.com>
2019-03-23 19:46:03 +0100

    add button and led view assignments to FW control panel

---
+++ Assets/Scripts/FWControlPanel/RightButtonView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RightButtonView : MonoBehaviour
+{
+    public Button btnBrandfallControlAb;
+    public Button btnBMZReset;
+    public Button UECheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/FWControlPanel/rightLEDView.cs
+++ Assets/Scripts/FWControlPanel/rightLEDView.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class rightLEDView : MonoBehaviour
+public class RightLEDView : MonoBehaviour
 {
     public GameObject panelUEexecuted;
     public GameObject panelBrandfallControlAb;
